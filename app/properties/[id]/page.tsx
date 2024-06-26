b3370da855e26e06615e1322ed822aa129917424
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-06-26 15:40:32 +0500

    add propertyCard

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
+import { useParams } from 'next/navigation'
 import React from 'react'
 
 const PropertiesPage = () => {
+	const { id } = useParams()
+
 	return (
 		<div>
-			<h1>Properties page</h1>
+			<h1>Properties page {id}</h1>
 		</div>
 	)
 }
---
+++ types.ts
@@ -0,0 +1,37 @@
+export type RootType = {
+	_id: string
+	owner: string
+	name: string
+	type: string
+	description: string
+	location: Location
+	beds: number
+	baths: number
+	square_feet: number
+	amenities: string[]
+	rates: Rates
+	seller_info: Sellerinfo
+	images: string[]
+	isFeatured: boolean
+	createdAt: string
+	updatedAt: string
+}
+
+export type Sellerinfo = {
+	name: string
+	email: string
+	phone: string
+}
+
+export type Rates = {
+	weekly: number
+	monthly?: number
+	nightly?: number
+}
+
+export type Location = {
+	street: string
+	city: string
+	state: string
+	zipcode: string
+}
