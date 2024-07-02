5ffe137e537060ee74f78caedefdf00019732441
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-07-02 12:57:43 +0500

    add property POST API route

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -13,3 +13,51 @@ export const GET = async (request: any) => {
 		return new Response('Someting Went Wrong', { status: 500 })
 	}
 }
+
+export const POST = async (request: any) => {
+	try {
+		const formData = await request.formData()
+
+		// Access all values from amenities and images
+		const amenities = formData.getAll('amenities')
+		const images = formData
+			.getAll('images')
+			.filter((image: any) => image.name !== '')
+
+		// Create propertyData object for database
+		const propertyData = {
+			type: formData.get('type'),
+			name: formData.get('name'),
+			description: formData.get('description'),
+			location: {
+				street: formData.get('location.street'),
+				city: formData.get('location.city'),
+				state: formData.get('location.state'),
+				zipcode: formData.get('location.zipcode'),
+			},
+			beds: formData.get('beds'),
+			baths: formData.get('baths'),
+			square_feet: formData.get('square_feet'),
+			amenities,
+			rates: {
+				weekly: formData.get('rates.weekly'),
+				monthly: formData.get('rates.monthly'),
+				nightly: formData.get('rates.nightly'),
+			},
+			seller_info: {
+				name: formData.get('seller_info.name'),
+				email: formData.get('seller_info.email'),
+				phone: formData.get('seller_info.phone'),
+			},
+			images,
+		}
+
+		console.log(propertyData)
+
+		return new Response(JSON.stringify({ messages: 'Success' }), {
+			status: 200,
+		})
+	} catch (error) {
+		return new Response(`Failed to add property ${error}`, { status: 500 })
+	}
+}
