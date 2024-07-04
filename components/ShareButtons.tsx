875b2bcc0d1a7466cd41c18b8e9d3d195a87a369
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-07-04 12:04:28 +0500

    property page cleanup

---
+++ components/ShareButtons.tsx
@@ -0,0 +1,17 @@
+import { RootType } from '@/types'
+import React, { FC } from 'react'
+import { FaShare } from 'react-icons/fa'
+
+type ShareButtonsType = {
+	property: RootType
+}
+
+const ShareButtons: FC<ShareButtonsType> = ({ property }) => {
+	return (
+		<button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+			<FaShare className='mr-2' /> Share Property
+		</button>
+	)
+}
+
+export default ShareButtons
