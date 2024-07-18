984ac03b559f61cade788aec06f03a349f344947
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-07-18 11:34:30 +0500

    add share buttons

--- components/ShareButtons.tsx
+++ components/ShareButtons.tsx
@@ -1,16 +1,53 @@
 import { RootType } from '@/types'
 import React, { FC } from 'react'
 import { FaShare } from 'react-icons/fa'
+import {
+	EmailIcon,
+	EmailShareButton,
+	FacebookIcon,
+	FacebookShareButton,
+	TelegramIcon,
+	TelegramShareButton,
+	TwitterIcon,
+	TwitterShareButton,
+	WhatsappIcon,
+	WhatsappShareButton,
+} from 'react-share'
 
 type ShareButtonsType = {
 	property: RootType
 }
 
 const ShareButtons: FC<ShareButtonsType> = ({ property }) => {
+	const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`
+
 	return (
-		<button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-			<FaShare className='mr-2' /> Share Property
-		</button>
+		<>
+			<h3 className='text-xl font-bold text-center pt-2'>
+				Share this property
+			</h3>
+			<div className='flex gap-3 justify-center pb-5'>
+				<TelegramShareButton url={shareUrl} title={property.name}>
+					<TelegramIcon size={40} round={true} />
+				</TelegramShareButton>
+
+				<WhatsappShareButton
+					url={shareUrl}
+					title={property.name}
+					separator=':: '
+				>
+					<WhatsappIcon size={40} round={true} />
+				</WhatsappShareButton>
+
+				<EmailShareButton
+					url={shareUrl}
+					subject={property.name}
+					body={`Check out this property listing: ${shareUrl}`}
+				>
+					<EmailIcon size={40} round={true} />
+				</EmailShareButton>
+			</div>
+		</>
 	)
 }
 
