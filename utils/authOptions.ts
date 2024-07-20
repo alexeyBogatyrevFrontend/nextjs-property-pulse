f2ab3b6dfba8b1af4caebcd0231a16fb739a6f45
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-07-20 13:05:25 +0500

    fix ts mistakes

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -6,7 +6,9 @@ import { signIn } from 'next-auth/react'
 export const authOptions = {
 	providers: [
 		GoogleProvider({
+			// @ts-ignore
 			clientId: process.env.GOOGLE_CLIENT_ID,
+			// @ts-ignore
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 			authorization: {
 				params: {
