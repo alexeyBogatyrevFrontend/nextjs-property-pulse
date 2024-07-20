b76296e85a7ac3ab7b08ad3a3edf01ca1a2730d0
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-07-20 12:20:09 +0500

    add photoswipe lightbox gallery

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/assets/css/globals.css'
+import 'react-toastify/dist/ReactToastify.css'
+import 'photoswipe/dist/photoswipe.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import AuthProvider from '@/components/AuthProvider'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 import { GlobalProvider } from '@/context/GlobalContext'
 
 const inter = Inter({ subsets: ['latin'] })
